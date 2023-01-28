849b399bc9aef3c6f9c83d5ff4da5074937626e1
= <=>
2023-01-27 20:43:07 -0600

    Add info about no account needed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <h1>{{ __('Book a shuttle') }}</h1>
+            
+            <div class="alert alert-info">
+                {{ __('To book a shuttle, you don\'t need an account - Just an email and a phone number.') }}
+            </div>
 
             <a href="{{ route('booking.new') }}" class="btn btn-info">{{ __('New booking') }}</a>
             <a href="{{ route('booking.load') }}" class="btn btn-info">{{ __('Modify/Cancel booking') }}</a>
